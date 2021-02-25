f2c64fe966594b0a0d053e486ed831817f2bb9b6
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-02-24 22:22:22 -0500

    added full crud

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,12 +5,13 @@ const methodOverride = require('method-override');
 const { requireToken } = require('../middleware/auth')
 
 
-
-router.post('/createblog', (req, res)=>{
+router.post('/createblog', (req, res)=>{ // create blog form
     console.log('route hit')
     Blog.create({
+        author: req.body.author,
         title: req.body.title,
-        content: req.body.content
+        content: req.body.content,
+        comment: req.body.comment
     })
     .then(user => {
         res.status(201).json(user)
@@ -20,24 +21,58 @@ router.post('/createblog', (req, res)=>{
     })
 })
 
-
-router.put('/updateblog', (req, res) => {
-    console.log('this route is getting hit')
-
+router.get('/seeblogs/:id',async (req,res)=> {
+    console.log('route hit')
+    console.log(req.params.id)
+    await Blog.findOne({
+        author: req.params.id
+    })
+    .then((blogs)=> {
+        console.log(blogs)
+        res.send(blogs)
+    })
+    .catch(err=>{
+        console.log(`this is error ${err}`)
+    })
 })
 
 
-router.post('/createcomment', requireToken, (req, res)=>{
+router.put('/updateblog/:id', async (req, res) => { // create update form
+    console.log('this route is getting hit')
+    await Blog.findByIdAndUpdate('6034000b84834f56c60b19d3', req.body, {new:true})
+    .then(response =>{
+        console.log(response)
+    })
+    .catch(err =>{
+        console.log(`this is an error ${err}`)
+    })
+})
+
+
+router.post('/createcomment', async(req, res)=>{ // create comment form
 
     console.log('route hit')
-    Blog.findById('6034000b84834f56c60b19d3')
+    await Blog.findById('6034000b84834f56c60b19d3')// req.params.id
     .then(foundBlog => {
-        foundBlog.comments.push({author: req.user.id , comment:req.body.comment })
+        console.log(foundBlog)
+        console.log(req)
+        console.log(req.body.comment)
+        foundBlog.comments.push({author: req.body.author , comment:req.body.comment })
         foundBlog.save()
         res.status(201).json(foundBlog)
     })
     .catch(err=>{
         console.log('this is an error', err)
+    })
+})
+
+router.delete('/deleteblog/:id', async (req, res) => { // connect to delete button
+    await Blog.findByIdAndDelete('603402d04c0e525730af4678')
+    .then(comment => {
+        console.log(comment)
+    })
+    .catch(err => {
+        console.log(`error message ${err}`)
     })
 })
 
